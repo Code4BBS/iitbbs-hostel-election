262b6c27217671a9728fc9ed6c9bf49daa4def32
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2022-02-13 21:44:34 +0530

    cors

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ const hostelRouter = require("./routes/hostelRoute");
 const app = express();
 
 app.use(helmet());
-app.use(
-  cors({
-    origin: "http://localhost:3001",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(xss());
 app.use(cookieParser());
 app.use(express.json({ limit: "10kb" }));
